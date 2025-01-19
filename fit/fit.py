34079b14dc0a6579e6416203ccddf5749d2f8747
nameduser aconite <nameduser@aconite.com>
2025-01-19 14:28:02 +0000

    fit.py crashed but have factored out input and output directories

---
+++ fit/fit.py
@@ -0,0 +1,22 @@
+from babl.architecture.T5.train import main  
+from babl.architecture.T5.data import prepare_dataset 
+from argparse import ArgumentParser
+from pathlib import Path 
+import warnings 
+
+warnings.filterwarnings("ignore")
+
+
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser()
+    parser.add_argument('--max-len', default=128)
+    parser.add_argument('--output-dir', default="ouputs")
+    parser.add_argument('--input-dir', default="inputs")
+    parser.add_argument("--root-dir", default=Path().cwd().parent)
+    parser.add_argument('--model-name-or-path', default='t5-small')
+    parser.add_argument('--target-max-len', default=32)
+    args = parser.parse_args()
+    main(args)
