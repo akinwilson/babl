ab5283b9bd757c93fe32a165aa7807d42b7b9d76
nameduser aconite <nameduser@aconite.com>
2025-01-19 20:04:36 +0000

    fitting

--- fit/fit.py
+++ fit/fit.py
@@ -13,9 +13,10 @@ if __name__ == "__main__":
 
     parser = ArgumentParser()
     parser.add_argument('--input-max-len', default=128)
-    parser.add_argument('--output-dir', default="ouputs")
+    parser.add_argument('--output-dir', default="outputs")
     parser.add_argument('--input-dir', default="inputs")
-    parser.add_argument("--root-dir", default=Path().cwd().parent)
+    print(Path(__file__).parent.parent)
+    parser.add_argument("--root-dir", default=Path(__file__).parent.parent)
     parser.add_argument('--model-name-or-path', default='t5-small')
     parser.add_argument('--output-max-len', default=32)
     args = parser.parse_args()
