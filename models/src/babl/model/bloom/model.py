86e229b9f9ec118e5bc2981fffd129008be95e60
nameduser aconite <nameduser@aconite.com>
2025-01-20 18:28:22 +0000

    factored into models.py

--- models/src/babl/model/bloom/model.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom")
-model = AutoModelForCausalLM.from_pretrained("bigscience/bloom")
--- models/src/babl/model/llama/model.py
+++
@@ -1,6 +0,0 @@
-
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.3-70B-Instruct")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.3-70B-Instruct")
-
