293ee96536dccb9266196a90b950b883f98a8b32
nameduser aconite <nameduser@aconite.com>
2025-01-20 09:28:25 +0000

    added start script to server

---
+++ serve/run_server.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+
+echo "Running server with either 2 or ${WORKERS} process workers . Set with WORKER env variable ..."
+echo "Running on port 80 with process worker lifetime of either 300 seconds or ${TIMEOUT}. Set with TIMEOUT env variable  ... "
+
+gunicorn -w ${WORKERS:=2} \ 
+  -b :80 -t ${TIMEOUT:=300} \
+  -k uvicorn.workers.UvicornWorker \
+  main:app
+
+echo "was WORKERTANK: ${WORKERSTANK:=2}"
