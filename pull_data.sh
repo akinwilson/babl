b5ec19ee0f7120cae662aa20e342c56be480ffaf
nameduser aconite <nameduser@aconite.com>
2025-01-28 13:59:38 +0000

    moved into app directory

--- pull_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Pulling training and validation datasets ..."
-mkdir -p inputs
-gdown --id 1enHDeeAySxoNIGvSew6Y5aFxBjEVe01w --output inputs/50k.jsonl
-gdown --id 1IuywHW-sjNDfMXssOimDwvvbeMaUKstq --output inputs/10k.jsonl
